2a5a5982543d1d4426ae025250e659245edd8b14
Alexis Indick <aindick@oswego.edu>
2020-12-24 11:54:42 -0500

    Added C File

---
+++ cmake-build-debug/CMakeFiles/UnixShell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnixShell.dir/Shell.c.obj"
+  "UnixShell.exe"
+  "UnixShell.exe.manifest"
+  "UnixShell.pdb"
+  "libUnixShell.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UnixShell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
